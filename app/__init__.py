f7bce6003c4e7a0a7b65d67e9e2923b16528175f
Lisian Shehu <rpglisi@gmail.com>
2020-10-16 23:09:21 -0400

    Updating run and init for factory application pattern implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,30 @@
 from flask import Flask
-from boto3 import dynamodb
+from flask_dynamo import Dynamo
+from flask_restplus import Api
 
 from .config import config_by_name
+from app.models.DynamoDB import DynamoDB
+from app.controllers.user_controller import api as user_ns
 
 
 def create_app(config_name):
-    # other configurations such as DB(Dynamo)
 
     app = Flask(__name__)
+    api = Api()
+    api.add_namespace(user_ns, path='/user')
     app.config.from_object(config_by_name[config_name])
+    app.config['DYNAMO_TABLES'] = [
+        dict(
+            TableName='Users',
+            KeySchema=[dict(AttributeName='user_id', KeyType='HASH')],
+            AttributeDefinitions=[dict(AttributeName='user_pass', AttributeType='S'),
+                                  dict(AttributeName='user_first_name', AttributeType='S'),
+                                  dict(AttributeName='user_last_name', AttributeType='S')]
+        )
+    ]
+    dynamo = Dynamo()
+    with app.app_context():
+        dynamo.init_app(app)
+        api.init_app(app)
 
     return app
