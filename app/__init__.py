72b25a362cb149a926718cb829d14618789213c9
Lisian Shehu <rpglisi@gmail.com>
2020-10-17 14:54:31 -0400

    Modifying main dynamoDB class and create_app() function to work with app factory method

--- app/__init__.py
+++ app/__init__.py
@@ -3,28 +3,31 @@ from flask_dynamo import Dynamo
 from flask_restplus import Api
 
 from .config import config_by_name
-from app.models.DynamoDB import DynamoDB
-from app.controllers.user_controller import api as user_ns
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
     api = Api()
-    api.add_namespace(user_ns, path='/user')
     app.config.from_object(config_by_name[config_name])
     app.config['DYNAMO_TABLES'] = [
         dict(
             TableName='Users',
-            KeySchema=[dict(AttributeName='user_id', KeyType='HASH')],
+            KeySchema=[dict(AttributeName='user_name', KeyType='HASH')],
             AttributeDefinitions=[dict(AttributeName='user_pass', AttributeType='S'),
                                   dict(AttributeName='user_first_name', AttributeType='S'),
                                   dict(AttributeName='user_last_name', AttributeType='S')]
-        )
+            # ProvisionedThroughput = dict(ReadCapacityUnits=5, WriteCapacityUnits=5)
+    )
     ]
+
     dynamo = Dynamo()
     with app.app_context():
         dynamo.init_app(app)
+
+        from app.controllers.user_controller import api as user_ns
+        api.add_namespace(user_ns, path='/user')
+
         api.init_app(app)
 
     return app
--- app/models/DynamoDB.py
+++ app/models/DynamoDB.py
@@ -1,9 +1,9 @@
-import boto3
 from flask import current_app
 
 
 class DynamoDB:
 
     def __init__(self):
-        self.tables = current_app.extensions['dynamodb']
+        self.tables = current_app.extensions['dynamo'].tables
+        print(self.tables)
 
