70ef0caff55457c412181721cef32b32a6e310df
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-10-25 14:59:17 -0400

    Adding checks for existing user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -7,6 +7,11 @@ class UserService:
         self.user_table = User().user_table
 
     def create_user(self, user_data):
+        item_response = self.user_table.get_item(Key={'user_name': user_data['user_name']})
+        if item_response is None:
+            print('User already exists')
+            return {'status': 'failed', 'message': 'User already exists'}, 409
+
         self.user_table.put_item(Item=user_data)
         response_object = {
             'status': 'success',
