fac88ad9d05c9d95890d73697ac3c24949401984
lisianshehu <rpglisi@gmail.com>
2020-11-25 23:36:09 -0500

    Adding service for logout, modifying login service and search service to incorporate updating of loginStatus in DB

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -22,7 +22,7 @@ class UserService:
         item_response = self.user_table.get_item(Key={'user_name': user_data['user_name']})
         if 'Item' in item_response:
             return {'status': 'failed', 'message': 'User already exists'}
-
+        user_data['loginStatus'] = False
         user_data['password'] = self.user_model.hash_password(user_data['password'])
         self.user_table.put_item(Item=user_data)
         response_object = {
@@ -34,25 +34,59 @@ class UserService:
     def login_user(self, user_data):
         user_name = user_data['user_name']
         password = user_data['password']
-        item_response = self.user_table.get_item(Key={'user_name': user_data['user_name']})
+        item_response = self.user_table.get_item(Key={'user_name': user_name})
         print(item_response)
         if 'Item' in item_response and self.user_model.check_password(self.get_user_password(user_name), password):
             expires = datetime.timedelta(days=7)
             auth_token = create_access_token(identity=str(user_name), expires_delta=expires)
             print("here: ", auth_token)
             if auth_token is not None:
+                self.user_table.update_item(
+                    Key={
+                        'user_name': user_name,
+                    },
+                    UpdateExpression="set loginStatus = :updated",
+                    ExpressionAttributeValues={
+                        ':updated': True,
+                    }
+                )
                 response = {'status': 'success', 'message': 'Successfully logged-in', 'token': auth_token}
                 print(response)
                 return response, 200
 
         return {'status': 'failed', 'message': 'Failed logged-in'}
 
+    def logout_user(self, user_data):
+        user_name = user_data['user_name']
+        table_response = self.user_table.update_item(
+            Key={
+                'user_name': user_name,
+            },
+            UpdateExpression="set loginStatus = :updated",
+            ExpressionAttributeValues={
+                ':updated': False,
+            }
+        )
+        print(table_response)
+        response = {'status': 'success', 'message': 'Logged-out!'}
+        return response
+
+    def get_online_status(self, user_to_search):
+        table_response = self.user_table.get_item(Key={'user_name': user_to_search})
+        if table_response['Item']['loginStatus'] == True:
+            print('Online {}'.format(table_response['Item']['loginStatus']))
+            return True
+
+        return False
+
     def search_user(self, user_to_search):
         item_response = self.user_table.get_item(Key={'user_name': user_to_search})
         if 'Item' in item_response:
             response = {'status': 'success', 'message': 'Search successful', 'username': item_response['Item']['user_name']}
-            return response
+            if self.get_online_status(user_to_search):
+                response['loginStatus'] = True
+                return response
         else:
-            return {'status': 'failed', 'message': 'User does not exist'}
+            return {'status': 'failed', 'message': 'User does not exist', 'loginStatus': True}
 
 
